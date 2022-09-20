84c384597970ada01206bb62e01ae1218d550a23
Dmitry <dmitrychinyaev93@yandex.ru>
2022-09-20 21:12:25 +0300

    project

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.mult(20,20));
+        System.out.println(calc.pow(3,3));
+        System.out.println(calc.sum(50,50));
+    }
+}
